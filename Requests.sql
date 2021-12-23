705a0bfdf859bfd7ebd7e23d490b3e593323e1cb
IT-02 Dmytro Kotenko <kotenko2002@gmail.com>
2021-12-23 17:44:36 +0200

    add trigger and 8 requests

--- Requests.sql
+++ Requests.sql
@@ -1,5 +1,25 @@
 USE shipping;
 
+-- вьюшка, зв'язуюча таблиці "Компанії перивізники" з "Кораблі"
+DROP VIEW IF EXISTS freightcompanies_ships;
+CREATE VIEW freightcompanies_ships
+AS
+SELECT freightcompanies.Id FC_Id, CountryCode, freightcompanies.Name FC_Name, PhoneNumber, Email,
+ships.Id S_Id, ShipTypeId, FreightCompanyId, ships.Name S_Name
+FROM ships 
+JOIN freightcompanies ON freightcompanies.Id = ships.FreightCompanyId;
+
+-- вьюшка, зв'язуюча таблиці "порти" з "контрактами" через табл "точки призначення"
+DROP VIEW IF EXISTS ports_contracts;
+CREATE VIEW ports_contracts
+AS
+SELECT ports.Id Ports_Id, CountryCode, ports.Name Ports_Name,
+contracts.Id Contracts_Id, ShipId, ClientId, SigningDate
+FROM destinations
+JOIN ports ON ports.Id = destinations.PortId
+JOIN contracts ON contracts.Id = destinations.ContractId;
+
+
 -- (1) топ 10 країн з найбільшою кількістю портів
 SELECT DISTINCT Countries.Name,  Ports.CountryCode, COUNT(Ports.CountryCode)
 FROM Ports
@@ -9,55 +29,49 @@ ORDER BY COUNT(Ports.CountryCode) DESC
 LIMIT 10;
  
 -- (2) топ 10 портів світу за відвідуваністю
-SELECT ports.CountryCode, ports.Name, COUNT(ports.Id) 
-FROM destinations
-JOIN ports ON ports.Id = destinations.PortId
-JOIN contracts ON contracts.Id = destinations.ContractId
-GROUP BY ports.Id
-ORDER BY ports.CountryCode DESC
-; 
- 
+SELECT CountryCode, Ports_Name, COUNT(Ports_Id) 
+FROM ports_contracts
+GROUP BY Ports_Id
+ORDER BY COUNT(Ports_Id)  DESC
+LIMIT 10;
+
 -- (3). топ 10 країн світу за відвідуваністю
-SELECT countries.Name, COUNT(ports.Id) 
-FROM destinations
-JOIN ports ON ports.Id = destinations.PortId
-JOIN contracts ON contracts.Id = destinations.ContractId
-JOIN countries ON countries.Code = ports.CountryCode
+SELECT countries.Name, COUNT(Ports_Id) 
+FROM ports_contracts
+JOIN countries ON countries.Code = CountryCode
 GROUP BY countries.Code
-ORDER BY COUNT(ports.Id) DESC
+ORDER BY COUNT(Ports_Id) DESC
 LIMIT 10;
  
 -- (4) кількість кораблів компаній перевізників
-SELECT CountryCode, freightcompanies.Name, COUNT(freightcompanies.Id) numberOfShips
-FROM ships
-JOIN freightcompanies ON freightcompanies.Id = ships.FreightCompanyId
-GROUP BY  freightcompanies.Id;
+SELECT CountryCode, FC_Name, COUNT(FC_Id) numberOfShips
+FROM freightcompanies_ships
+GROUP BY  FC_Id;
  
 -- (5) кількість співробітників компаній перевізників на кораблях
-SELECT CountryCode, freightcompanies.Name, COUNT(workers.Id) numberOfWorkers
+SELECT CountryCode, FC_Name, COUNT(workers.Id) numberOfWorkers
 FROM workers
-JOIN ships ON ships.Id = workers.ShipId
-JOIN freightcompanies ON freightcompanies.Id = ships.FreightCompanyId
-GROUP BY freightcompanies.Id;
+JOIN freightcompanies_ships ON S_Id = workers.ShipId
+GROUP BY FC_Id;
  
 -- (6) контрактна інформація компанії перевізника та компаній, які користуються її послугами
-SELECT freightcompanies.Name Freight_Company_Name, freightcompanies.PhoneNumber FK_PhoneNumber, freightcompanies.Email FK_Email,
+SELECT FC_Name Freight_Company_Name, freightcompanies_ships.PhoneNumber FC_PhoneNumber, freightcompanies_ships.Email FC_Email,
 clients.Name Client_Company_Name, clients.PhoneNumber CC_PhoneNumber, clients.Email CC_Email
 FROM contracts
-JOIN ships ON ships.Id = contracts.ShipId
+JOIN freightcompanies_ships ON S_Id = contracts.ShipId
 JOIN clients ON clients.Id = contracts.ClientId
-JOIN freightcompanies ON freightcompanies.Id = ships.FreightCompanyId
-GROUP BY freightcompanies.Id, clients.Id
-ORDER BY freightcompanies.Id;
+GROUP BY FC_Id, clients.Id
+ORDER BY FC_Id;
  
 -- (7) інформація про завантаженість кожного корабля 
-SELECT  ships.Name, SUM(products.Weight) CurrentWeight, 
+SELECT ships.Name, SUM(products.Weight) CurrentWeight, 
 shiptypes.MaxWeight - SUM(products.Weight) FreeWeight, shiptypes.MaxWeight
 FROM products
 JOIN contracts ON contracts.Id = products.ContractId
-JOIN ships ON ships.Id = contracts.ShipId
+RIGHT JOIN ships ON ships.Id = contracts.ShipId
 JOIN shiptypes ON shiptypes.Id = ships.ShipTypeId
-GROUP BY ships.Id;
+GROUP BY ships.Id
+order by ships.Id;
  
 -- (8) повна інформація про продукт
 SELECT products.Name ProductName, clients.Name ClientName, Weight, Price
@@ -80,11 +94,10 @@ JOIN ships ON ships.Id = workers.ShipId
 WHERE ships.Name = 'The Hateful Terror';
  
 -- (11)  інформація, які порти/країни корабель повинен відвідати
-SELECT ports.Name, countries.Name FROM ships
-JOIN contracts ON ships.Id = contracts.ShipId
-JOIN destinations ON contracts.Id = destinations.ContractId
-JOIN ports ON ports.Id = destinations.PortId
-JOIN countries ON countries.Code = ports.CountryCode
+SELECT Ports_Name, countries.Name Country_Name
+FROM ships
+JOIN ports_contracts ON ships.Id = ShipId
+JOIN countries ON countries.Code = CountryCode
 WHERE ships.Name = 'The Hateful Terror';
 
 -- (12)  інформація, про всіх "назва професії" в компанії "назва компанії"
@@ -95,13 +108,66 @@ JOIN workers ON ships.Id =  workers.ShipId
 JOIN positions ON positions.Id = workers.PositionId
 WHERE freightcompanies.Name = 'A.P. Moller–Maersk Group' AND positions.Name = 'Капітан';
 
+-- (13) компанії, продукцію яких буде перевозити корабель по Id корабля
+SELECT clients.Name, SigningDate FROM ships
+JOIN contracts ON contracts.ShipId = ships.Id
+JOIN clients ON clients.Id = contracts.ClientId
+WHERE ships.Id = 3;
 
-SELECT freightcompanies.Name, COUNT(freightcompanies.Id) numberOfShips
+ -- (14)інформація, в яких країнах побуває працівник корабля по Id працівника
+SELECT DISTINCT countries.Name 
+FROM workers
+JOIN ships ON ships.Id = workers.ShipId
+JOIN ports_contracts ON ships.Id = ports_contracts.ShipId
+JOIN countries ON countries.Code = CountryCode
+WHERE workers.Id = 347;
+
+-- (15) вся продукція, що перевозиться кораблем по Id корабля
+SELECT products.Name, Weight, Price 
 FROM ships
-JOIN freightcompanies ON freightcompanies.Id = ships.FreightCompanyId
-GROUP BY  freightcompanies.Id, ships.ShipTypeId;
+JOIN contracts ON contracts.ShipId = ships.Id
+JOIN products ON contracts.Id = products.ContractId
+WHERE ships.Id = 3;
 
-SELECT ShipTypeId, COUNT(ShipTypeId) FROM shipping.ships
-GROUP BY ShipTypeId;
+-- (16) інформація про кількість працівників, загальну суму ЗА, та середню ЗП в компанії по Id компанії
+SELECT  COUNT(workers.Id) Toatal_workers, SUM(workers.Salary) Total_Salary, 
+ROUND(SUM(workers.Salary)/COUNT(workers.Id),2) AVG_Salary
+FROM workers
+JOIN freightcompanies_ships ON S_Id = workers.ShipId
+WHERE FC_Id = 2;
 
-SELECT COUNT(ShipTypeId) FROM ships;
+-- (17) прибуток компаній перевізників за останні "n" місяців
+SELECT freightcompanies.Name, SUM(products.Price) FROM freightcompanies
+JOIN ships ON ships.FreightCompanyId = freightcompanies.Id
+JOIN contracts ON contracts.ShipId = ships.Id
+JOIN products ON contracts.Id = products.ContractId
+WHERE MONTH(NOW()) - MONTH(SigningDate) < 5
+GROUP BY freightcompanies.Id;
+
+-- (18) прибутки перевізника за різні місяці по Id компанії
+SELECT freightcompanies.Name, SUM(products.Price), MONTHNAME(SigningDate) FROM freightcompanies
+JOIN ships ON ships.FreightCompanyId = freightcompanies.Id
+JOIN contracts ON contracts.ShipId = ships.Id
+JOIN products ON contracts.Id = products.ContractId
+WHERE freightcompanies.Id = 6
+group by MONTH(SigningDate)
+ORDER BY MONTH(SigningDate);
+
+-- (19) кораблі, що не мають жодного контракту
+SELECT S_Id Ship_Id, S_Name Ship_Name, 
+shiptypes.Name Type_Name, shiptypes.MaxWeight, FC_Name Owner_Name
+FROM freightcompanies_ships
+JOIN shiptypes ON shiptypes.Id = ShipTypeId
+WHERE NOT EXISTS 
+(SELECT * FROM contracts WHERE contracts.ShipId = S_Id)
+ORDER BY S_Id;
+
+-- (20) кораблі, що не мають жодного продукту на борту
+SELECT ships.Name Ship_Name, shiptypes.Name Type, shiptypes.MaxWeight 
+FROM products
+JOIN contracts ON contracts.Id = products.ContractId
+RIGHT JOIN ships ON ships.Id = contracts.ShipId
+JOIN shiptypes ON shiptypes.Id = ships.ShipTypeId
+GROUP BY ships.Id
+HAVING SUM(products.Weight) IS NULL
+order by ships.Id;
